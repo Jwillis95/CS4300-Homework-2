9fbd1ae793147fd31f79f23eae0cd0d634a2e395
Joel <jiwillis95@gmail.com>
2025-02-26 18:27:37 +0000

    Finished implementing MVT architecture. Specifically, created the HTML templates.

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MovieViewSet, SeatViewSet, BookingViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet)
@@ -9,4 +10,8 @@ router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', views.movie_list, name='movie_list'),
+    path('movie/<int:movie_id>/book/', views.seat_booking, name='seat_booking'),
+    path('booking/history/', views.booking_history, name='booking_history'),
 ]
+
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 from .models import Movie, Seat, Booking
 from .serializers import MovieSerializer, SeatSerializer, BookingSerializer
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from .models import Booking
 
 
 # Create your views here.
@@ -16,3 +19,36 @@ class SeatViewSet(viewsets.ModelViewSet):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+def movie_list(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_list.html', {'movies': movies})
+
+@login_required
+def seat_booking(request, movie_id):
+    movie = Movie.objects.get(id=movie_id)
+    seats = Seat.objects.filter(movie=movie)
+
+    if request.method == 'POST':
+        selected_seat_id = request.POST.get('seat')
+        selected_seat = Seat.objects.get(id=selected_seat_id)
+        selected_seat_booked = True
+        selected_seat.save()
+
+        Booking.objects.create(
+            movie=movie,
+            seat=selected_seat,
+            user=request.user,
+            booking_date=request.POST.get('booking_date')
+        )
+        return render(request, 'seat_booking.html', { 'movie': movie, 'seats': seats, 'message': 'Booking Successful'})
+
+    return render(request, 'seat_booking.html', {'movie': movie, 'seats': seats})
+
+    @login_required
+    def booking_history(request):
+        bookings = Bookings.objects.filter(user=request.user)
+        return render(request, 'booking_history.html', { 'bookings': bookings})
+def booking_history(request):
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'booking_history.html', {'bookings': bookings})
